156a58a64d9f782b2b9883444f93baa723a7c633
Tatsuya Matsui <tatchan50@hotmail.com>
2020-07-24 17:31:41 +0900

    Add numerical value extensions class.

--- DataExtensions.cs
+++ DataExtensions.cs
@@ -132,5 +132,161 @@ namespace my_library_for_cs
                 return (TEnum)Enum.ToObject(typeof(TEnum), value);
             }
         }
+
+        /// <summary>
+        /// 数値拡張クラス
+        /// </summary>
+        public static class NumericalValueExtensions
+        {
+            /// <summary>
+            /// T型の整数をBCDに変換する※符号つき変数用
+            /// </summary>
+            /// <typeparam name="T">型</typeparam>
+            /// <param name="num">整数</param>
+            /// <param name="byteCount">バイト数</param>
+            /// <returns></returns>
+            private static byte[] Any2BCD<T>(T num, int byteCount) where T : struct, IConvertible
+            {
+                long value = Convert.ToInt64(num);
+                byte[] bcd = new byte[byteCount];
+                for (int i = 1; i <= byteCount; i++)
+                {
+                    long mod = value % 100;
+                    long digit2 = mod % 10;
+                    long digit1 = (mod - digit2) / 10;
+                    bcd[byteCount - i] = Convert.ToByte((digit1 * 16) + digit2);
+                    value = (value - mod) / 100;
+                }
+                return bcd;
+            }
+
+            /// <summary>
+            /// T型の整数をBCDに変換する※符号なし変数用
+            /// </summary>
+            /// <typeparam name="T">型</typeparam>
+            /// <param name="num">整数</param>
+            /// <param name="byteCount">バイト数</param>
+            /// <returns></returns>
+            private static byte[] UAny2BCD<T>(T num, int byteCount) where T : struct, IConvertible
+            {
+                ulong value = Convert.ToUInt64(num);
+                byte[] bcd = new byte[byteCount];
+                for (int i = 1; i <= byteCount; i++)
+                {
+                    ulong mod = value % 100;
+                    ulong digit2 = mod % 10;
+                    ulong digit1 = (mod - digit2) / 10;
+                    bcd[byteCount - i] = Convert.ToByte((digit1 * 16) + digit2);
+                    value = (value - mod) / 100;
+                }
+                return bcd;
+            }
+
+            #region Int <-> BCD
+            /// <summary>
+            /// BCDをInt型の整数に変換する
+            /// </summary>
+            /// <param name="bcd">BCD</param>
+            /// <returns></returns>
+            public static int BCD2Int(byte[] bcd)
+            {
+                return Convert.ToInt32(BCD2Long(bcd));
+            }
+
+            /// <summary>
+            /// Int型の整数をBCDに変換する
+            /// </summary>
+            /// <param name="num">整数</param>
+            /// <param name="byteCount">バイト数</param>
+            /// <returns></returns>
+            public static byte[] Int2BCD(int num, int byteCount)
+            {
+                return Any2BCD<int>(num, byteCount);
+            }
+            #endregion Int <-> BCD
+
+            #region UInt <-> BCD
+            /// <summary>
+            /// BCDをUInt型の整数に変換する
+            /// </summary>
+            /// <param name="bcd">BCD</param>
+            /// <returns></returns>
+            public static uint BCD2UInt(byte[] bcd)
+            {
+                return Convert.ToUInt32(BCD2ULong(bcd));
+            }
+
+            /// <summary>
+            /// UInt型の整数をBCDに変換する
+            /// </summary>
+            /// <param name="num">整数</param>
+            /// <param name="byteCount">バイト数</param>
+            /// <returns></returns>
+            public static byte[] UInt2BCD(uint num, int byteCount)
+            {
+                return UAny2BCD<uint>(num, byteCount);
+            }
+            #endregion UInt <-> BCD
+
+            #region Long <-> BCD
+            /// <summary>
+            /// BCDをLong型の整数に変換する
+            /// </summary>
+            /// <param name="bcd">BCD</param>
+            /// <returns></returns>
+            public static long BCD2Long(byte[] bcd)
+            {
+                long result = 0;
+                foreach (byte b in bcd)
+                {
+                    int digit1 = b >> 4;
+                    int digit2 = b & 0x0f;
+                    result = (result * 100) + (digit1 * 10) + digit2;
+                }
+                return result;
+            }
+
+            /// <summary>
+            /// Long型の整数をBCDに変換する
+            /// </summary>
+            /// <param name="num">整数</param>
+            /// <param name="byteCount">バイト数</param>
+            /// <returns></returns>
+            public static byte[] Long2BCD(long num, int byteCount)
+            {
+                return Any2BCD<long>(num, byteCount);
+            }
+            #endregion Long <-> BCD
+
+            #region ULong <-> BCD
+            /// <summary>
+            /// BCDをULong型の整数に変換する
+            /// </summary>
+            /// <param name="bcd">BCD</param>
+            /// <returns></returns>
+            public static ulong BCD2ULong(byte[] bcd)
+            {
+                ulong result = 0;
+                foreach (byte b in bcd)
+                {
+                    uint digit1 = (uint)b >> 4;
+                    uint digit2 = (uint)b & 0x0f;
+                    result = (result * 100) + (digit1 * 10) + digit2;
+                }
+                return result;
+            }
+
+            /// <summary>
+            /// ULong型の整数をBCDに変換する
+            /// </summary>
+            /// <param name="num">整数</param>
+            /// <param name="byteCount">バイト数</param>
+            /// <returns></returns>
+            public static byte[] ULong2BCD(ulong num, int byteCount)
+            {
+                return UAny2BCD<ulong>(num, byteCount);
+            }
+            #endregion ULong <-> BCD
+        }
     }
 }
